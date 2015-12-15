2ec0633958a66e19c5fa633dbd6e2a4e0ea271bf
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2015-12-15 19:57:35 +0530

    Babel-node presets order fixed!

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -2,4 +2,4 @@ cite 'about-alias'
 about-alias 'Personal alises'
 
 alias a='atom . && echo Opening Atom in \"$(pwd)/\"'
-alias es='babel-node --presets "stage-1,es2015"'
+alias es='babel-node --presets "es2015,stage-1"'
