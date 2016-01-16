5818b783f60ad74a16db4a50ed6fb830a128a47a
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-01-16 10:52:23 +0530

    Sorted aliases and added auto-approving aptupgd alias.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -1,7 +1,8 @@
 cite 'about-alias'
 about-alias 'Personal alises'
 
-alias git=hub
 alias a='atom . && echo Opening Atom in \"$(pwd)/\"'
+alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove'
 alias es='babel-node --presets "es2015,stage-1"'
+alias git=hub
 alias npmup='sudo npm update -g'
