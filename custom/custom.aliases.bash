1e90986afa1c3bef46643a5bda00639968f739ad
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-07-15 22:19:26 +0530

    Changed ito NCDU.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -3,7 +3,7 @@ about-alias 'Personal alises'
 
 alias a='atom . && echo Opening Atom in \"$(pwd)/\"'
 alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove'
-alias ds='du -sh'
+alias ds='ncdu'
 alias es='babel-node --presets "es2015,stage-1"'
 alias git=hub
 alias npmup='sudo npm update -g'
