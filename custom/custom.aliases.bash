77618fc28d318d24a487ca7525530dc3d894ff50
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-09-04 09:53:58 +0530

    Removed redundant quotes and escapes from existing aliases.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -1,9 +1,9 @@
 cite 'about-alias'
 about-alias 'Personal alises'
 
-alias a='atom . && echo Opening Atom in \"$(pwd)/\"'
+alias a='atom . && echo Opening Atom in "$(pwd)/"'
 alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove'
-alias ds='ncdu'
+alias ds=ncdu
 alias es='babel-node --presets "es2015,stage-1"'
 alias git=hub
 alias npmup='sudo npm update -g'
