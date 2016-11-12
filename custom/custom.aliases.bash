90647278e612f1bab9eb45afdf40760a2551ebf2
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-11-12 15:34:30 +0530

    Custom aliases updated.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -4,7 +4,14 @@ about-alias 'Personal alises'
 alias a='atom . && echo Opening Atom in "$(pwd)/"'
 alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove'
 alias ds=ncdu
-alias es='babel-node --presets "es2015,stage-1"'
 alias git=hub
 alias npmup='sudo npm update -g'
 alias pipup='sudo pip install -U $(pip list | cut -d " " -f 1)'
+
+# Compile and run various fossil platforms for the masters. Remove when not needed.
+gccr () {
+  gcc -o ${1%.*} $1 && ./${1%.*}
+}
+javar () {
+  javac $1 && java ${1%.*}
+}
