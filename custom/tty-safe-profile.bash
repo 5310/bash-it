c2294c9ffc1b5f99925c505ffdc60fecd9741fc8
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2016-11-30 20:02:00 +0530

    Node plugin disused, tty-safe profile remade.

---
+++ custom/tty-safe-profile.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+###
+# This profile is TTY-safe.
+# I use it as my .bash_profile`
+# which gets loaded by login shells
+# much like the TTY accessible from ctrl+alt+fN
+###
+
+# Path to the bash it configuration
+export BASH_IT="/home/scio/.bash_it"
+
+# Inherit the default .bashrc
+source $BASH_IT/.bashrc
+
+# Lock and Load a custom theme file
+# location /.bash_it/themes/
+export BASH_IT_THEME='powerline-naked-newline'
+
+# Load Bash It
+source $BASH_IT/bash_it.sh
