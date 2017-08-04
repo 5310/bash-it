675fd95ab2d46330f4f13e9a48c3f1e7c1a92520
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-08-04 11:11:50 +0530

    Exa alias added.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -7,6 +7,7 @@ alias ds=ncdu
 alias git=hub
 alias npmup='sudo npm update -g'
 alias pipup='sudo pip install -U $(pip list | cut -d " " -f 1)'
+alias x='exa -bhHlS --git'
 
 # Compile and run various fossil platforms for the masters. Remove when not needed.
 gccr () {
