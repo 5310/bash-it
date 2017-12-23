9cfe65e5b78c45f108ad86b61c16f1c9b52a77df
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-12-23 12:54:21 +0530

    Auto-yes for apt autoremove.

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -2,7 +2,7 @@ cite 'about-alias'
 about-alias 'Personal alises'
 
 alias a='atom . && echo Opening Atom in "$(pwd)/"'
-alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove'
+alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -Vy && sudo apt-get autoremove -y'
 alias ds=ncdu
 alias git=hub
 alias npmup='sudo npm update -g'
