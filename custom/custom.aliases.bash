49a46195246fd9e313a9d5d8d1d67d9a472af147
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-08-12 11:09:46 +0530

    Fixed aptupgd, added bat, exa, pnpmup

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -2,11 +2,14 @@ cite 'about-alias'
 about-alias 'Personal alises'
 
 alias a='atom . && echo Opening Atom in "$(pwd)/"'
-alias aptupgd='sudo aptitude update && sudo aptitude dist-upgrade -Vy -o Aptitude::Delete-Unused=1'
+alias aptupgd='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y'
+alias cat='bat'
 alias ds=ncdu
 alias git=hub
 alias npmup='sudo npm update -g'
 alias pipup='sudo pip install -U $(pip list | cut -d " " -f 1)'
+alias pnpmup='sudo pnpm update -g'
+alias xt='exa -bhHlST --git'
 alias x='exa -bhHlS --git'
 
 # Compile and run various fossil platforms for the masters. Remove when not needed.
