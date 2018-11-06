82086d2fad9fa90aaf9f2f6782b13572e6e665be
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-11-06 11:38:57 +0530

    Updated pnpmup to also update pnpm

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -8,7 +8,7 @@ alias ds=ncdu
 alias git=hub
 alias npmup='sudo npm update -g'
 alias pipup='sudo pip install -U $(pip list | cut -d " " -f 1)'
-alias pnpmup='sudo pnpm upgrade -g'
+alias pnpmup='sudo pnpm i -g pnpm && sudo pnpm upgrade -g'
 alias xt='exa -bhHlST --git'
 alias x='exa -bhHlS --git'
 
