53afa2ffe53162d1b96493c16529c5f3b717279c
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-09-15 19:53:37 +0530

    Customize aliases

--- custom/custom.aliases.bash
+++ custom/custom.aliases.bash
@@ -1,19 +1,19 @@
 cite 'about-alias'
 about-alias 'Personal alises'
 
-alias e='code . && echo Opening VS Code in "$(pwd)/"'
+alias e='nano'
 alias aptupgd='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove --purge -y'
 alias cat='bat --paging never'
 alias ds=ncdu
 #alias git=hub
 alias npmup='sudo npm update -g'
 alias pipup='sudo pip install -U $(pip list | cut -d " " -f 1 | tail -n +3)'
-alias pnpmup='sudo pnpm i -g pnpm && sudo pnpm install -g $(pnpm list -g --json | fx this[0].dependencies "Object.keys(this).join(\" \")")' # depends on npm:fx
+#alias pnpmup='sudo pnpm i -g pnpm && sudo pnpm install -g $(pnpm list -g --json | fx this[0].dependencies "Object.keys(this).join(\" \")")' # depends on npm:fx
 #alias pnpmup='sudo pnpm i -g pnpm && sudo pnpm install -g $(pnpm list -g --json | grep '"from":' | cut -d " " -f 10 | cut -c 2- | rev | cut -c 3- | rev)' # plain shell cleanup
-alias xt='exa -bhHlST --git'
-alias x='exa -bhHlS --git'
-alias temp="sensors | grep Core | cut -d + -f 2 | cut -d '.' -f 1 | tr '\r\n' ' ' && nvidia-smi -q -d temperature | grep 'GPU Current Temp' | cut -d ':' -f 2 | cut -d ' ' -f 2"
-alias temps='echo "c0 c1 c2 c3 g0" && while true; do temp; sleep 2; done'
+#alias xt='exa -bhHlST --git'
+#alias x='exa -bhHlS --git'
+#alias temp="sensors | grep Core | cut -d + -f 2 | cut -d '.' -f 1 | tr '\r\n' ' ' && nvidia-smi -q -d temperature | grep 'GPU Current Temp' | cut -d ':' -f 2 | cut -d ' ' -f 2"
+#alias temps='echo "c0 c1 c2 c3 g0" && while true; do temp; sleep 2; done'
 
 # Compile and run various fossil platforms for the masters. Remove when not needed.
 gccr () {
